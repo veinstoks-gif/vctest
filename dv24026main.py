8c86cbb6a31536f74b934c984339ea318cd791af
Didzis <veinstoks@gmail.com>
2025-12-17 18:39:09 +0200

    izlabots import

--- dv24026main.py
+++ dv24026main.py
@@ -1,7 +1,7 @@
 # This is a simple calculation program
 # created to demonstrate version control.
-from libadd import *
-from libmult import *
+from dv24026add import *
+from dv24026mult import *
 a = 13
 b = 5
 print(plus(a,b))
